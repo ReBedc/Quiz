18d816d3faa52ae380cba84dbafbab1daa7058e4
Rebeca D <rebekadc@gmail.com>
2015-08-15 21:18:42 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,45 @@
-// GET /quizes/question
-exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function (quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next (new Error('No existe quizId = ' + quizId));
+			}
+		}
+	).catch(function (error) {next(error);});
 };
 
-// GET /quizes/answer 
+// GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+			res.render('index.ejs', {title: 'Quiz', quizes: quizes});
+		}
+	).catch(function (error) {next(error);});
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+	res.render('quizes/question.ejs', {quiz: req.quiz});
+};
+
+// GET /quizes/:id/answer 
 exports.answer = function(req, res) {
-	if (req.query.respuesta === 'Roma') {
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = 'Correcto';
 	}
-	else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});	
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( // creación de un nuevo objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+
+	res.render('quizes/new', {quiz: quiz});
 };
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bienvenido a Quiz' });
+router.get('/', function(req, res) {
+  res.render('bienvenido', {title: 'Quiz'});
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+// Autoload
+router.param('quizId', quizController.load);
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+//router.get('/quizes/new', quizController.new);
+//router.get('quizes/create', quizController.create);
 
 router.get('/author', function(req, res, next) {
   res.render('author');
