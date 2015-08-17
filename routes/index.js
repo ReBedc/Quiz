ede370bc8ec347381dbcb130969e7b5ff48f8cb7
Rebeca D <rebekadc@gmail.com>
2015-08-17 18:11:24 +0200

    ejercicio modulo 8

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bienvenido a Quiz' });
+router.get('/', function(req, res) {
+  res.render('bienvenido', {title: 'Quiz', errors: []});
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+// Autoload
+router.param('quizId', quizController.load);
 
-router.get('/author', function(req, res, next) {
-  res.render('author');
-});
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+
+
 
 module.exports = router;
